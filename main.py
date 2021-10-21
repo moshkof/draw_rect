d9e7bce9261ef2774788d6329c368ae2c1cf309e
cataclysm78 <cataclysm78@yandex.ru>
2021-10-21 17:56:30 +0300

    "Добавлен текст"

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ image = cv2.resize(image, (1280, 720))
 pt1 = (450, 40)
 pt2 = (800, 420)
 
-img_rect = cv2.rectangle(image, pt1, pt2, (0, 255, 0), 2, cv2.LINE_4)
+img_rect = cv2.rectangle(image, pt1, pt2, (0, 255, 0), 1, cv2.LINE_4)
+
+text = 'Vouuu...'
+org = (455, 70)
+fontFace = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+color = (0, 255, 14)
+linType = cv2.LINE_4
+
+img_text = cv2.putText(img_rect, text, org, fontFace, fontScale, color, linType)
 
 cv2.imshow('Rect', img_rect)
 
